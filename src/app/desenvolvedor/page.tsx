8dab0b9e66396f36c64147069127c22776642e6a
JJByte30 <joaojosejunior030@gmail.com>
2025-08-18 14:50:20 -0300

    comecei a colocar os textos

--- src/app/desenvolvedor/page.tsx
+++ src/app/desenvolvedor/page.tsx
@@ -3,48 +3,47 @@ import ButtonNext from "@/components/buttonnext";
 import Profile from "@/components/perfil/profile";
 import Link from "next/link";
 
-interface Sobre {
-    titulo: string,
-    texto: string
-}
-
-export default function Desenvolvedor () {
-    const lista: Sobre[] =  [
-        {
-            titulo: "Historia",
-            texto: "texto"
-        },
-        {
-            titulo: "Experiencias",
-            texto: "texto"
-        },
-        {
-            titulo: "Linguagens",
-            texto: "texto"
-        }
-        
-    ]
+export default function Desenvolvedor() {
     return (
         <section id="home" className="min-h-screen flex bg-gradient-to-br from-black via-[#200704] to-black relative overflow-hidden">
             <div className="flex flex-col mt-16 gap-6 max-w-[1200px] mx-auto px-8 z-10">
                 <div className="flex flex-col items-center">
-                    <Profile/>
+                    <Profile />
                     <h2 className="text-lg font-extrabold uppercase tracking-wider text-center bg-gradient-to-r from-[#B9030F] to-white bg-clip-text text-transparent">Desenvolvedor</h2>
                 </div>
-                <div className="">
-                    <ul className="flex flex-col tracking-wider list-disc marker:text-white gap-y-4">
-                        {lista.map(({titulo, texto}, index) => {
-                            return (
-                                <li key={index}>
-                                    <h2 className="font-semibold text-white">{titulo}</h2>
-                                    <p className="text-white">{texto}</p>
-                                </li>
-                            )
-                        })}
+                <article>
+                    <ul className="flex flex-col tracking-wider marker:text-white gap-y-4 mx-6 mb-4">
+                        <li>
+                            <h2 className="font-semibold text-white">- Historia:</h2>
+                            <p className="text-white whitespace-pre-line">Meu primeiro contato com programação aconteceu em 2018/2019, durante um curso na Bit+ Uberaba, onde comecei a entender lógica de programação e dar os primeiros passos no mundo da tecnologia. Alguns anos depois, iniciei a graduação em Análise e Desenvolvimento de Sistemas, que consolidou minha base de conhecimento em desenvolvimento web, banco de dados e programação orientada a objetos. Desde então, venho aprimorando minhas habilidades e direcionando minha carreira para o desenvolvimento de sistemas modernos e eficientes.</p>
+                        </li>
+                        <li>
+                            <h2 className="font-semibold text-white">- Experiencias:</h2>
+                            <p className="text-white whitespace-pre-line">Entre setembro de 2024 e setembro de 2025, atuei na Codiub (Companhia de Desenvolvimento de Informática de Uberaba), onde tive a oportunidade de trabalhar em diferentes frentes do desenvolvimento de software.</p>
+                            <p className="text-white whitespace-pre-line">Essa experiência me permitiu evoluir tanto em aspectos técnicos quanto em arquitetura de aplicações web, boas práticas de programação e integração entre sistemas.</p>
+                            <p className="text-white whitespace-pre-line">Passei por Angular no frontend e Java (Spring Boot) no backend, mas meu principal foco foi o desenvolvimento com React e Next.js, criando interfaces modernas, performáticas e alinhadas às boas práticas do mercado.</p>
+                        </li>
+                        <li className="flex flex-col gap-2">
+                            <h2 className="font-semibold text-white">- Linguagens:</h2>
+                            <div className="flex flex-col text-sm md:flex-row gap-2">
+                                <div className="">
+                                    <p className="font-semibold">Front-end:</p>
+                                    <p className="text-white"> React, Next.js, Angular, JavaScript, TypeScript, HTML5, CSS3 (Tailwind)</p>
+                                </div>
+                                <div>
+                                    <p className="font-semibold">Back-end:</p>
+                                    <p className="text-white">Java (Spring Boot)</p>
+                                </div>
+                                <div>
+                                    <p className="font-semibold">Banco de Dados:</p>
+                                    <p className="text-white">SQL, MySQL, PostgreSQL</p>
+                                </div>
+                            </div>
+                        </li>
                     </ul>
-                </div>
+                </article>
             </div>
-            <ButtonNext proximo = "/editor" anterior = "/direito"/>
+            <ButtonNext proximo="/editor" anterior="/direito" />
         </section>
     );
 }
--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -3,48 +3,45 @@ import ButtonNext from "@/components/buttonnext";
 import Profile from "@/components/perfil/profile";
 import Link from "next/link";
 
-interface Sobre {
-    titulo: string,
-    texto: string
-}
-
-export default function Editor () {
-        const lista: Sobre[] =  [
-        {
-            titulo: "Historia",
-            texto: "texto"
-        },
-        {
-            titulo: "Experiencias",
-            texto: "texto"
-        },
-        {
-            titulo: "Linguagens",
-            texto: "texto"
-        }
-        
-    ]
+export default function Editor() {
     return (
         <section id="home" className="min-h-screen flex bg-gradient-to-br from-black via-[#200704] to-black relative overflow-hidden">
             <div className="flex flex-col mt-16 gap-6 max-w-[1200px] mx-auto px-8 z-10">
                 <div className="flex flex-col items-center">
-                    <Profile/>
+                    <Profile />
                     <h2 className="text-lg font-extrabold uppercase tracking-wider text-center bg-gradient-to-r from-[#B9030F] to-white bg-clip-text text-transparent">Editor de Vídeo</h2>
                 </div>
-                <div className="">
-                    <ul className="flex flex-col tracking-wider list-disc marker:text-white gap-y-4">
-                        {lista.map(({titulo, texto}, index) => {
-                            return (
-                                <li key={index}>
-                                    <h2 className="font-semibold text-white">{titulo}</h2>
-                                    <p className="text-white">{texto}</p>
-                                </li>
-                            )
-                        })}
+                <article>
+                    <ul className="flex flex-col tracking-wider marker:text-white gap-y-4 mx-6 mb-4">
+                        <li className="flex flex-col gap-1">
+                            <h2 className="font-semibold text-white">- Historia:</h2>
+                            <p className="text-white whitespace-pre-line">Tenho cerca de <span className="font-semibold">10 a 11 anos de experiência em edição de vídeo</span>, começando muito cedo, por volta dos <span className="font-semibold">8/9 anos de idade</span>, utilizando o <span className="font-semibold">Camtasia Studio</span> para criar vídeos pessoais para o YouTube, especialmente sobre <span className="font-semibold">Minecraft</span>. Na época, possuía um canal com cerca de <span className="font-semibold">10 mil seguidores</span> e gerenciava meu próprio servidor, programando o básico e adicionando plugins.</p>
+                            <p className="text-white whitespace-pre-line">Posteriormente, migrei para o <span className="font-semibold">Sony Vegas</span>, onde continuei editando vídeos até meus <span className="font-semibold">14 anos</span>, produzindo conteúdos para o YouTube e aprimorando técnicas de corte, transições e efeitos.</p>
+                            <p className="text-white whitespace-pre-line">A partir dessa idade, comecei a me aprofundar em edições mais profissionais utilizando o <span className="font-semibold">After Effects</span>, produzindo vídeos para meus perfis no Instagram (<span className="font-semibold">hunter.wmv</span> e <span className="font-semibold">zw4g,g</span>), explorando efeitos avançados, animações e diversos plugins.</p>
+                            <p className="text-white whitespace-pre-line">Além disso, ao longo do tempo, também aprimorei habilidades em <span className="font-semibold">Photoshop</span> e <span className="font-semibold">Adobe Premiere</span>, ampliando meu leque de edição e composição visual.</p>
+                        </li>
+                        <li className="flex flex-col gap-1">
+                            <h2 className="font-semibold text-white">- Experiencias:</h2>
+                            <p className="text-white whitespace-pre-line">Trabalhei tanto em projetos pessoais quanto em trabalhos freelance, produzindo edições personalizadas para outras pessoas e empresas virtuais, como a <span className="font-semibold">Suricato{"'"}x Corporation</span> (desenvolvimento de mods para <span className="font-semibold">FiveM</span>), onde fui responsável por vídeos e banners.</p>
+                            <p className="text-white whitespace-pre-line">Mais recentemente, a partir de <span className="font-semibold">2024</span>, criei um <span className="font-semibold">TikTok</span> de fã das séries <span className="font-semibold">Supernatural</span> e <span className="font-semibold">Suits</span>, produzindo edits que chegaram a monetizar a página. Atualmente, minha conta <span className="font-semibold">blazz.aep</span> conta com <span className="font-semibold">70 mil seguidores</span>, consolidando minha experiência em produção de conteúdo audiovisual para redes sociais.</p>
+                        </li>
+                        <li className="flex flex-col gap-1">
+                            <h2 className="font-semibold text-white">- Plataformas e Plugins:</h2>
+                            <div className="flex flex-col text-sm md:flex-row gap-4">
+                                <div>
+                                    <p className="font-semibold">Plataformas:</p>
+                                    <p className="text-white"><span className="font-medium">After Effects, Adobe Premiere, Photoshop, Sony Vegas, Camtasia Studio</span></p>
+                                </div>
+                                <div>
+                                    <p className="font-semibold">Plugins & Efeitos:</p>
+                                    <p className="text-white"><span className="font-medium">Magic Bullet, Sapphire, S_Shark, Red Giant Universe, Trapcode Particular, Optical Flares, Element 3D, Twitch, Saber, transições personalizadas, Color Grading, Motion Graphics, animações tipográficas</span></p>
+                                </div>
+                            </div>
+                        </li>
                     </ul>
-                </div>
+                </article>
             </div>
-            <ButtonNext proximo = "/social" anterior = "/desenvolvedor"/>
+            <ButtonNext proximo="/social" anterior="/desenvolvedor" />
         </section>
     );
 }
